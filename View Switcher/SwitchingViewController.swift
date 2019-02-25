501b95de5207be342e07b25ccb695b140feaaf35
Sean Klechak <sklechak@students.mchenry.edu>
2019-02-24 18:11:16 -0600

    Corrected
    Corrected yellow transition Issue

--- View Switcher/SwitchingViewController.swift
+++ View Switcher/SwitchingViewController.swift
@@ -47,20 +47,30 @@ class SwitchingViewController: UIViewController {
             } else if blueViewController?.view.superview == nil {
                 if blueViewController == nil {
                     blueViewController = storyboard?.instantiateViewController(withIdentifier: "Blue") as? BlueViewController
-                } else {
-                    blueViewController.view.frame = view.frame
-                    switchViewController(from: yellowViewController, to: blueViewController)
+//                } else {
+//                    blueViewController.view.frame = view.frame
+//                    switchViewController(from: yellowViewController, to: blueViewController)
+                    }
                 }
-            }  // switch view controllers
+            UIView.beginAnimations("View Flip", context: nil)
+            UIView.setAnimationDuration(0.4)
+            UIView.setAnimationCurve(.easeInOut)
+        
+            // switch view controllers
             if blueViewController != nil
                 && blueViewController!.view.superview != nil {
+                UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+                
                 yellowViewController.view.frame = view.frame
                 switchViewController(from: blueViewController, to: yellowViewController)
             } else {
+                UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
                 blueViewController.view.frame = view.frame
                 switchViewController(from: yellowViewController, to: blueViewController)
             }
+        UIView.commitAnimations()
     }
+    
     private func switchViewController(from fromVC:UIViewController?, to toVC:UIViewController?) {
         if fromVC != nil {
             fromVC!.willMove(toParent: nil)
